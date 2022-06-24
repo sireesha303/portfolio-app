4ae45fdebdf6c661c4defb7bc7ebca9c4e25fe0b
sireesha <msireesha30031999@gmail.com>
2022-06-24 17:39:17 +0530

    added basic component structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+
+import Home from "./components/Home"
+import Skills from "./components/Skills"
+import Projects from "./components/Projects"
+import WorkExperience from "./components/WorkExperience"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+
+  return(
+    <BrowserRouter>
+      <Routes>
+          <Route to="/" element={<Home />} />
+          <Route to="/skills" element={<Skills />} />
+          <Route to="/projects" element={<Projects />} />
+          <Route to="/experience" element={<WorkExperience/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+
 }
 
 export default App;
